265e77c7ee2ed3d31e91dee1c74078fdb1c6dfe4
veloukarthik <71749429+veloukarthik@users.noreply.github.com>
2024-11-20 07:22:14 +0530

    todo list paginations added

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit,OnChanges,OnDestroy,AfterContentChecked,AfterContentInit,DoCheck,AfterViewChecked,AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy, AfterContentChecked, AfterContentInit, DoCheck, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 
 @Component({
@@ -7,52 +7,67 @@ import { TodoService } from '../services/todo.service';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-    title:string = 'Todo List';
-    todos:any[] = [];
-    todoTitle:string = '';
-    todo:any;
-    todoEdit:boolean = false;
+  title: string = 'Todo List';
+  todos: any[] = [];
+  todoTitle: string = '';
+  todo: any;
+  todoEdit: boolean = false;
+  limit: number = 10;
+  page: number = 0;
+  count: number = 0;
 
-    constructor(private todoService:TodoService)
-    { 
+  constructor(private todoService: TodoService) {
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  AddTodo() {
+    let count = this.todos.length + 1;
+    this.todos.push({ id: count, title: this.todoTitle, completed: false });
+    this.todoTitle = '';
+  }
+
+  editTodo(todo: any) {
+    this.todoTitle = todo.title;
+    this.todo = todo;
+    this.todoEdit = true;
+  }
+
+  update() {
+    this.todo.title = this.todoTitle;
+    this.todoEdit = false;
+    this.todoTitle = '';
+    this.todos = [...this.todos];
+  }
+
+  ngOnInit() {
+    this.todos = [];
+    this.getTodos();
+  }
+
+  prev() {
+    if (this.page > 0) {
+      this.page = this.page - this.limit;
+      console.log("prev",this.page);
+      this.getTodos();
+
     }
 
-    removeTodo(index:number)
-    {
-      this.todos.splice(index, 1);
-    }
-
-    AddTodo()
-    {
-      let count = this.todos.length+1;
-      this.todos.push({id:count,title:this.todoTitle,completed:false});
-      this.todoTitle = '';
-    }
-
-    editTodo(todo:any)
-    {
-        this.todoTitle = todo.title;
-        this.todo = todo;
-        this.todoEdit = true;
-    }
-
-    update()
-    {
-      this.todo.title = this.todoTitle;
-      this.todoEdit = false;
-      this.todoTitle = '';
-      this.todos = [...this.todos];
-    }
-
-    ngOnInit(){
-      this.todos = [];
+  }
+  next() {
+    if (this.page < this.count-10) {
+      this.page = this.page + this.limit;
+      console.log("next",this.page);
       this.getTodos();
     }
+  }
 
-    getTodos()
-    {
-        this.todoService.getTodos().subscribe((data) => {
-          this.todos = data.splice(0,10);
-        });
-    }
+  getTodos() {
+    this.todoService.getTodos().subscribe((data) => {
+      this.count = data.length;
+      this.todos = data.splice(this.page, this.limit);
+    });
+  }
 }
